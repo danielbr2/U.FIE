aae9dd342bad619dd2b7fbbce7979ec44490b51c
6801318d8d <144167388+6801318d8d@users.noreply.github.com>
2025-11-03 06:39:24 +0100

    Update Example-13-3-1.R

--- R/13/Example-13-3-1.R
+++ R/13/Example-13-3-1.R
@@ -1,9 +1,10 @@
 data(kielmc, package='wooldridge')
 
-# Separate regressions for 1978 and 1981: report coeeficients only
+# Separate regressions for 1978 and 1981: report coefficients only
 coef( lm(rprice~nearinc, data=kielmc, subset=(year==1978)) )
 coef( lm(rprice~nearinc, data=kielmc, subset=(year==1981)) )
 
 # Joint regression including an interaction term 
 library(lmtest)
 coeftest( lm(rprice~nearinc*y81, data=kielmc) )
+
