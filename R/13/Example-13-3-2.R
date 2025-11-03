55c2af3de570b11ce9130a14e80df6e99c5b073b
6801318d8d <144167388+6801318d8d@users.noreply.github.com>
2025-11-03 06:36:20 +0100

    Update Example-13-3-2.R

--- R/13/Example-13-3-2.R
+++ R/13/Example-13-3-2.R
@@ -1,5 +1,8 @@
+data(kielmc, package='wooldridge')
+
 DiD      <- lm(log(rprice)~nearinc*y81                     , data=kielmc)
 DiDcontr <- lm(log(rprice)~nearinc*y81+age+I(age^2)+log(intst)+
                             log(land)+log(area)+rooms+baths, data=kielmc)
 library(stargazer)
 stargazer(DiD,DiDcontr,type="text")
+
